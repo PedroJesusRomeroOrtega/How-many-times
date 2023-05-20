cbb4bc8a8f927f252177a4a50f5436a153bfcc76
Pedro Romero <pedrojesusromeroortega@gmail.com>
2023-05-20 19:03:48 +0200

    Add new product group modal

--- src/app/product-group-list/product-group-list.component.ts
+++ src/app/product-group-list/product-group-list.component.ts
@@ -1,26 +1,48 @@
-import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewChild,
+  signal,
+} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {IonicModule} from '@ionic/angular';
+import {IonModal, IonicModule} from '@ionic/angular';
+import {OverlayEventDetail} from '@ionic/core/components';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-product-group-list',
   standalone: true,
-  imports: [CommonModule, IonicModule],
+  imports: [CommonModule, IonicModule, FormsModule],
   templateUrl: './product-group-list.component.html',
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProductGroupListComponent {
+  @ViewChild(IonModal) modal!: IonModal;
+  name = '';
   productGroups = signal<ProductGroup[]>([
     {name: 'Kitesurf'},
     {name: 'Bikes'},
     {name: 'Scuba'},
   ]);
 
-  addProductGroup() {
-    this.productGroups.mutate((productGroups) =>
-      productGroups.push({name: 'PaddleSurf'})
-    );
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    this.modal.dismiss(this.name, 'confirm');
+  }
+
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm') {
+      this.addProductGroup(ev.detail.data || '');
+    }
+  }
+
+  private addProductGroup(name: string) {
+    this.productGroups.mutate((productGroups) => productGroups.push({name}));
   }
 }
 
