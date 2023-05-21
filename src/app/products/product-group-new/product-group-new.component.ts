b9005084edd9a94910a14687e56b85e5451e4f0d
Pedro Romero <pedrojesusromeroortega@gmail.com>
2023-05-21 19:04:27 +0200

    Add validations

--- src/app/products/product-group-new/product-group-new.component.ts
+++ src/app/products/product-group-new/product-group-new.component.ts
@@ -1,26 +1,38 @@
-import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {FormsModule} from '@angular/forms';
+import {FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
 import {IonicModule, ModalController} from '@ionic/angular';
+import {errorMessage} from 'src/app/shared';
 
 @Component({
   selector: 'app-product-group-new',
   standalone: true,
-  imports: [CommonModule, IonicModule, FormsModule],
+  imports: [CommonModule, IonicModule, ReactiveFormsModule],
   templateUrl: './product-group-new.component.html',
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductGroupNewComponent {
+export class ProductGroupNewComponent implements OnInit {
+  MAX_PRODUCTGROUP_NAME_LENGTH = 30;
   @Input() name = '';
+  productGroupName = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3),
+    Validators.maxLength(this.MAX_PRODUCTGROUP_NAME_LENGTH),
+  ]);
+  errorMessage = errorMessage;
 
   constructor(private modalCtrl: ModalController) {}
+
+  ngOnInit(): void {
+    this.productGroupName.setValue(this.name);
+  }
 
   cancel() {
     return this.modalCtrl.dismiss(null, 'cancel');
   }
 
   confirm() {
-    return this.modalCtrl.dismiss(this.name, 'confirm');
+    return this.modalCtrl.dismiss(this.productGroupName.value, 'confirm');
   }
 }
---
+++ src/app/shared/errors.ts
@@ -0,0 +1,16 @@
+import {FormControl} from '@angular/forms';
+
+export const defaultErrors = new Map<string, string>();
+defaultErrors.set('required', 'Is required');
+defaultErrors.set('minlength', 'Text must be longer');
+defaultErrors.set('maxlength', 'Text must be shorter');
+
+export const errorMessage = (formControl: FormControl): string => {
+  const errors = formControl.errors;
+  if (errors) {
+    const firstKey = Object.keys(errors)[0];
+    const getError = defaultErrors.get(firstKey);
+    return getError || '';
+  }
+  return '';
+};
