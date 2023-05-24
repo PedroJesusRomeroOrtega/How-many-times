5a3ea771988d51262371d6dd31822aab0755aa03
Pedro Romero <pedrojesusromeroortega@gmail.com>
2023-05-24 18:47:48 +0200

    Add productgroup list to route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./products/product-group-list/product-group-list.component').then(
+        (m) => m.ProductGroupListComponent
+      ),
+  },
+];
